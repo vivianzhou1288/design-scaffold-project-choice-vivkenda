49b59d6093fb7c04d4bfebab4abf03e839cfa638
vivianzhou1288 <87792254+vivianzhou1288@users.noreply.github.com>
2022-03-31 10:12:42 -0400

    Commit

--- vite-project/js/cuisine.js
+++ vite-project/js/cuisine.js
@@ -1,2 +1,64 @@
 import "../styles/cuisine.css";
 import { gsap } from "gsap";
+
+const food = [
+  {
+    img: "../images/964.jpg",
+    name: "Cong You Bing",
+  },
+  {
+    img: "../images/963.jpg",
+    name: "Peking Duck",
+  },
+  {
+    img: "../images/965.jpg",
+    name: "Red Braised Pork Belly",
+  },
+  {
+    img: "../images/966.jpg",
+    name: "Stir-Fried Rice Cakes",
+  },
+  {
+    img: "../images/967.jpg",
+    name: "Chow Mein",
+  },
+  {
+    img: "../images/1032.jpg",
+    name: "Sichuan Mala Chicken",
+  },
+  {
+    img: "../images/998.jpg",
+    name: "Mapo Tofu",
+  },
+  {
+    img: "../images/1033.jpg",
+    name: "Fried Lobster",
+  },
+  {
+    img: "../images/999.jpg",
+    name: "Fried Dumplings",
+  },
+  {
+    img: "../images/9910.jpg",
+    name: "Chinese Sauerkraut Boiled Fish",
+  },
+  {
+    img: "../images/9911.jpg",
+    name: "Fried Rice",
+  },
+  {
+    img: "../images/9912.jpg",
+    name: "Noodles with Beef and Cucumber",
+  },
+  {
+    img: "../images/9913.jpg",
+    name: "Buddha Jumps Over the Wall",
+  },
+  {
+    img: "../images/9914.jpg",
+    name: "Drunken Noodles",
+  },
+  {
+    img: "../images",
+  },
+];
