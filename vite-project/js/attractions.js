cf58c2378bbaaa3fb29a1b83fbc780121e3f7248
Kendi45 <90659938+Kendi45@users.noreply.github.com>
2022-04-05 10:12:23 -0400

    1

--- vite-project/js/attractions.js
+++ vite-project/js/attractions.js
@@ -1,2 +1,14 @@
 import "../styles/attractions.css";
 import { gsap } from "gsap";
+
+gsap.to(".box", 1, {
+  scale: 1,
+  opacity: 1,
+  yoyo: true,
+  y: 60,
+  ease: "power1.inOut",
+  stagger: {
+    amount: 1.5,
+    from: "left",
+  },
+});
