03785b14fcfb121c2e1180bccd7e11a9dd6ceb95
Kendi45 <90659938+Kendi45@users.noreply.github.com>
2022-05-05 22:01:34 -0400

    groot-Slideshow

--- vite-project/js/groot.js
+++ vite-project/js/groot.js
@@ -1,3 +1,37 @@
 import "../styles/groot.css";
 import { gsap } from "gsap";
 
+var slideIndex = 1;
+showDivs(slideIndex);
+
+const DOMSelectors = {
+  left: document.querySelector(".left"),
+  right: document.querySelector(".right"),
+};
+
+DOMSelectors.left.addEventListener("click", function () {
+  plusDivs(-1);
+});
+
+DOMSelectors.right.addEventListener("click", function () {
+  plusDivs(1);
+});
+
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex - 1].style.display = "block";
+}
