53b65111b5a4e0666f9de6edb51c5557b10503ee
Kendi45 <90659938+Kendi45@users.noreply.github.com>
2022-05-26 09:52:42 -0400

    1

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,10 +5,10 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(_dirname, "index.html"),
-        nested: resolve(_dirname, "attractions.html"),
-        nested: resolve(_dirname, "cuisine.html"),
-        nested: resolve(_dirname, "culture.html"),
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "attractions.html"),
+        nested: resolve(__dirname, "cuisine.html"),
+        nested: resolve(__dirname, "culture.html"),
       },
     },
   },
