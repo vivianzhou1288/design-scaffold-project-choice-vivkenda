c0801098c42d0d803397b78399bdd20e740d99ad
Kendi45 <90659938+Kendi45@users.noreply.github.com>
2022-05-26 23:14:28 -0400

    Media Queries

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -5,10 +5,8 @@ module.exports = defineConfig({
   build: {
     rollupOptions: {
       input: {
-        main: resolve(_dirname, "index.html"),
-        nested: resolve(_dirname, "attractions.html"),
-        nested: resolve(_dirname, "cuisine.html"),
-        nested: resolve(_dirname, "culture.html"),
+        main: resolve(__dirname, "index.html"),
+        nested: resolve(__dirname, "culture.html"),
       },
     },
   },
