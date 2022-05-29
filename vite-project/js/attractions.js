847cbca485745f60035530c5bb769a1ab3e50984
Kendi45 <90659938+Kendi45@users.noreply.github.com>
2022-05-29 19:13:32 -0400

    Revert "1"
    This reverts commit 4686a9d5b8c255ffca7bf5acea3944c65761b646.

--- vite-project/js/attractions.js
+++ vite-project/js/attractions.js
@@ -68,24 +68,3 @@ buttons.forEach((button) => {
     delete activeSlide.dataset.active;
   });
 });
-const navSlide = () => {
-  const menu = document.querySelector(".toggle-button");
-  const nav = document.querySelector(".nav-links");
-  const navLinks = document.querySelector(".nav-links li");
-
-  menu.addEventListener("click", () => {
-    nav.classList.toggle("nav-active");
-    console.log(Object.keys(navLinks));
-    Object.keys(navLinks).forEach((link, index) => {
-      if (link.style.animation) {
-        link.style.animation = "";
-      } else {
-        link.style.animation = `navLinkFade 0.5s ease forwards ${
-          index / 7 + 0.3
-        }s`;
-      }
-    });
-    menu.classList.toggle("toggle");
-  });
-};
-navSlide();
