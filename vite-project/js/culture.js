acf10bb2fe1b13a75696c6b9f18360675f48779b
vivianzhou1288 <vivianzhou1288@gmail.com>
2022-05-29 23:14:40 -0400

    js

--- vite-project/js/culture.js
+++ vite-project/js/culture.js
@@ -139,3 +139,24 @@ gsap.from(".winter-solistice", {
   ease: "circ.out",
   stagger: 0.5,
 });
+const navSlide = () => {
+  const menu = document.querySelector(".toggle-button");
+  const nav = document.querySelector(".nav-links");
+  const navLinks = document.querySelector(".nav-links li");
+
+  menu.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    console.log(Object.keys(navLinks));
+    Object.keys(navLinks).forEach((link, index) => {
+      if (link.style.animation) {
+        link.style.animation = "";
+      } else {
+        link.style.animation = `navLinkFade 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+    menu.classList.toggle("toggle");
+  });
+};
+navSlide();
