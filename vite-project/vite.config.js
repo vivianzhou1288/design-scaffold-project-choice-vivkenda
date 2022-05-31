a16c82061fff34290c97a55c8993c70f08c8e336
vivianzhou1288 <vivianzhou1288@gmail.com>
2022-05-30 20:05:54 -0400

    commit

--- vite-project/vite.config.js
+++ vite-project/vite.config.js
@@ -6,9 +6,12 @@ module.exports = defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, "index.html"),
-        nested: resolve(__dirname, "culture.html"),
-        nested: resolve(__dirname, "cuisine.html"),
-        nested: resolve(__dirname, "attractions.html"),
+        nested: resolve(
+          __dirname,
+          "culture.html",
+          "cuisine.html",
+          "attractions.html"
+        ),
       },
     },
   },
